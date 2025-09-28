b90aaed4b018e784464b20533f22ae4c6da62602
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-09-28 20:01:24 +0200

    feat: basic class providers

--- src/oauth/provider/index.ts
+++ src/oauth/provider/index.ts
@@ -1,2 +1,192 @@
 export * from "./error";
 export * from "./internal";
+
+import type { JWK, JWKSet } from "unjwt";
+import { isPublicJWK } from "unjwt/utils";
+
+import type {
+  OAuthProviderOptions,
+  ResolvedAuthorizationCodeOptions,
+  ResolvedAccessTokenOptions,
+  ResolvedRefreshTokenOptions,
+  BuildOAuthDiscoveryArgs,
+  BuildAuthorizationCodeArgs,
+  BuildAuthorizationCodeGrantArgs,
+  BuildClientCredentialsGrantArgs,
+  BuildRefreshTokenGrantArgs,
+} from "./internal";
+import {
+  oauthProviderDefaults,
+  buildOAuthDiscoveryDocument,
+  buildAuthorizationCode,
+  validateRedirectUri,
+  validateTokenRequest,
+  buildAuthorizationCodeGrant,
+  buildClientCredentialsGrant,
+  buildRefreshTokenGrant,
+  introspectAuthorizationCode,
+  introspectAccessToken,
+  introspectRefreshToken,
+} from "./internal";
+
+/**
+ * OAuthProvider
+ *
+ * A thin convenience wrapper over the low-level helpers exported from this module.
+ * It shares a common issuer and token options across authorize, token, and
+ * introspection helpers. All standalone helpers remain exported above; this class
+ * is purely ergonomic and does not add new logic.
+ */
+export class OAuthProvider {
+  readonly iss: string;
+  private readonly acOptions: ResolvedAuthorizationCodeOptions;
+  private readonly rtOptions: ResolvedRefreshTokenOptions;
+  private readonly atOptions: ResolvedAccessTokenOptions;
+  private readonly randomJti: () => string;
+  readonly accessTokenJwkSet: JWKSet;
+  readonly jwkSet: JWKSet;
+
+  constructor(args: OAuthProviderOptions) {
+    const opts = oauthProviderDefaults(args);
+
+    this.iss = opts.issuer;
+    this.randomJti = opts.randomJti;
+    this.acOptions = opts.authorizationCodeOptions;
+    this.rtOptions = opts.refreshTokenOptions;
+    this.atOptions = opts.accessTokenOptions;
+    this.accessTokenJwkSet = getPublicKeys(this.atOptions.publicKey);
+    this.jwkSet = this.accessTokenJwkSet;
+  }
+
+  // Discovery
+
+  discovery(options?: Omit<BuildOAuthDiscoveryArgs, "issuer">) {
+    return buildOAuthDiscoveryDocument({ ...options, issuer: this.iss });
+  }
+
+  // Authorize
+
+  validateAuthorizeRedirectUri(
+    redirectUri: string | undefined,
+    registeredRedirectUris: string | string[],
+    state?: string,
+  ) {
+    return validateRedirectUri(
+      { redirectUri, state },
+      registeredRedirectUris,
+      this.iss,
+    );
+  }
+
+  async authorizationCode(
+    args: Pick<BuildAuthorizationCodeArgs, "req" | "claims">,
+  ) {
+    return buildAuthorizationCode({
+      req: args.req,
+      claims: args.claims,
+      iss: this.iss,
+      randomJti: this.randomJti,
+      options: this.acOptions,
+    });
+  }
+
+  // Token
+
+  validateTokenRequest(req: unknown) {
+    return validateTokenRequest(req, this.iss);
+  }
+
+  async authorizationCodeGrant(
+    args: Omit<
+      BuildAuthorizationCodeGrantArgs,
+      "iss" | "randomJti" | "accessTokenOptions" | "refreshTokenOptions"
+    >,
+  ) {
+    return buildAuthorizationCodeGrant({
+      req: args.req,
+      codeClaims: args.codeClaims,
+      currentDate: args.currentDate,
+      extraAccessTokenClaims: args.extraAccessTokenClaims,
+      extraRefreshTokenClaims: args.extraRefreshTokenClaims,
+      iss: this.iss,
+      randomJti: this.randomJti,
+      accessTokenOptions: this.atOptions,
+      refreshTokenOptions: this.rtOptions,
+    });
+  }
+
+  async clientCredentialsGrant(
+    args: Omit<
+      BuildClientCredentialsGrantArgs,
+      "iss" | "randomJti" | "accessTokenOptions"
+    >,
+  ) {
+    return buildClientCredentialsGrant({
+      req: args.req,
+      currentDate: args.currentDate,
+      extraAccessTokenClaims: args.extraAccessTokenClaims,
+      iss: this.iss,
+      randomJti: this.randomJti,
+      accessTokenOptions: this.atOptions,
+    });
+  }
+
+  async refreshTokenGrant(
+    args: Omit<
+      BuildRefreshTokenGrantArgs,
+      "iss" | "randomJti" | "accessTokenOptions" | "refreshTokenOptions"
+    >,
+  ) {
+    return buildRefreshTokenGrant({
+      req: args.req,
+      currentDate: args.currentDate,
+      refreshTokenClaims: args.refreshTokenClaims,
+      extraAccessTokenClaims: args.extraAccessTokenClaims,
+      extraRefreshTokenClaims: args.extraRefreshTokenClaims,
+      iss: this.iss,
+      randomJti: this.randomJti,
+      accessTokenOptions: this.atOptions,
+      refreshTokenOptions: this.rtOptions,
+    });
+  }
+
+  // Introspection
+
+  introspectAuthorizationCode(token: string) {
+    return introspectAuthorizationCode({
+      token,
+      iss: this.iss,
+      options: this.acOptions,
+    });
+  }
+
+  introspectAccessToken(token: string) {
+    return introspectAccessToken({
+      token,
+      iss: this.iss,
+      options: this.atOptions,
+    });
+  }
+
+  introspectRefreshToken(token: string) {
+    return introspectRefreshToken({
+      token,
+      iss: this.iss,
+      options: this.rtOptions,
+    });
+  }
+}
+
+function getPublicKeys(publicKey?: JWK | JWK[]): JWKSet {
+  if (publicKey !== undefined) {
+    const key = Array.isArray(publicKey)
+      ? publicKey
+      : [publicKey];
+
+    return {
+      keys: key.filter((key) => key.kid !== undefined && isPublicJWK(key)),
+    };
+  }
+
+  return { keys: [] };
+}
--- src/oidc/provider/index.ts
+++ src/oidc/provider/index.ts
@@ -1,1 +1,170 @@
 export * from "./internal";
+
+import type { JWK, JWKSet } from "unjwt";
+import { isPublicJWK } from "unjwt/utils";
+
+import { OAuthProvider } from "../../oauth";
+import type {
+  ResolvedAccessTokenOptions,
+  ResolvedAuthorizationCodeOptions,
+  ResolvedRefreshTokenOptions,
+} from "../../oauth/provider/internal";
+
+import type {
+  OIDCProviderOptions,
+  ResolvedIdTokenOptions,
+  BuildAuthorizationCodeArgs,
+  BuildAuthorizationCodeGrantArgs,
+  BuildAuthorizationCodeGrantReturn,
+  BuildAuthorizationCodeReturn,
+  BuildOIDCDiscoveryArgs,
+  BuildRefreshTokenGrantArgs,
+  BuildRefreshTokenGrantReturn,
+  OIDCDiscoveryDocument,
+  OIDCUserInfoProfile,
+} from "./internal";
+import {
+  oidcProviderDefaults,
+  buildAuthorizationCode,
+  buildAuthorizationCodeGrant,
+  buildOIDCDiscoveryDocument,
+  buildRefreshTokenGrant,
+  buildUserInfo,
+  introspectIdToken,
+} from "./internal";
+
+export class OIDCProvider extends OAuthProvider {
+  private readonly authorizationCodeOptions: ResolvedAuthorizationCodeOptions;
+  private readonly refreshTokenOptions: ResolvedRefreshTokenOptions;
+  private readonly accessTokenOptions: ResolvedAccessTokenOptions;
+  private readonly idTokenOptions: ResolvedIdTokenOptions;
+  private readonly randomJtiFn: () => string;
+  readonly idTokenJwkSet: JWKSet;
+  override readonly jwkSet: JWKSet;
+
+  constructor(args: OIDCProviderOptions) {
+    const { idTokenOptions, ...defaults } = oidcProviderDefaults(args);
+
+    super(defaults);
+    this.authorizationCodeOptions = defaults.authorizationCodeOptions;
+    this.refreshTokenOptions = defaults.refreshTokenOptions;
+    this.accessTokenOptions = defaults.accessTokenOptions;
+    this.randomJtiFn = defaults.randomJti;
+    this.idTokenJwkSet = getPublicKeys(idTokenOptions.publicKey);
+    this.jwkSet = mergeUniqueKids(this.accessTokenJwkSet, this.idTokenJwkSet);
+
+    this.idTokenOptions = idTokenOptions;
+  }
+
+  // Discovery
+
+  override discovery(
+    options?: Omit<BuildOIDCDiscoveryArgs, "issuer">,
+  ): OIDCDiscoveryDocument {
+    return buildOIDCDiscoveryDocument({ ...options, issuer: this.iss });
+  }
+
+  buildUserInfo<T extends Record<string, unknown>>(
+    profile: OIDCUserInfoProfile,
+  ) {
+    return buildUserInfo<T>(profile);
+  }
+
+  // Authorize
+
+  override async authorizationCode(
+    args: Pick<BuildAuthorizationCodeArgs, "req" | "claims">,
+  ): Promise<BuildAuthorizationCodeReturn> {
+    return buildAuthorizationCode({
+      req: args.req,
+      claims: args.claims,
+      iss: this.iss,
+      randomJti: this.randomJtiFn,
+      options: this.authorizationCodeOptions,
+    });
+  }
+
+  // Token
+
+  override async authorizationCodeGrant(
+    args: Omit<
+      BuildAuthorizationCodeGrantArgs,
+      | "iss"
+      | "randomJti"
+      | "accessTokenOptions"
+      | "refreshTokenOptions"
+      | "idTokenOptions"
+    >,
+  ): Promise<BuildAuthorizationCodeGrantReturn> {
+    return buildAuthorizationCodeGrant({
+      ...args,
+      iss: this.iss,
+      randomJti: this.randomJtiFn,
+      accessTokenOptions: this.accessTokenOptions,
+      refreshTokenOptions: this.refreshTokenOptions,
+      idTokenOptions: this.idTokenOptions,
+    });
+  }
+
+  override async refreshTokenGrant(
+    args: Omit<
+      BuildRefreshTokenGrantArgs,
+      | "iss"
+      | "randomJti"
+      | "accessTokenOptions"
+      | "refreshTokenOptions"
+      | "idTokenOptions"
+    >,
+  ): Promise<BuildRefreshTokenGrantReturn> {
+    return buildRefreshTokenGrant({
+      ...args,
+      iss: this.iss,
+      randomJti: this.randomJtiFn,
+      accessTokenOptions: this.accessTokenOptions,
+      refreshTokenOptions: this.refreshTokenOptions,
+      idTokenOptions: this.idTokenOptions,
+    });
+  }
+
+  // Introspection
+
+  introspectIdToken(token: string) {
+    return introspectIdToken({
+      token,
+      iss: this.iss,
+      options: this.idTokenOptions,
+    });
+  }
+}
+
+function getPublicKeys(publicKey?: JWK | JWK[]): JWKSet {
+  if (publicKey !== undefined) {
+    const key = Array.isArray(publicKey)
+      ? publicKey
+      : [publicKey];
+
+    return {
+      keys: key.filter((key) => key.kid !== undefined && isPublicJWK(key)),
+    };
+  }
+
+  return { keys: [] };
+}
+
+function mergeUniqueKids(a: JWKSet, b: JWKSet): JWKSet {
+  const map = new Map<string, JWK>();
+
+  for (const key of a.keys) {
+    if (key.kid !== undefined) {
+      map.set(key.kid, key);
+    }
+  }
+
+  for (const key of b.keys) {
+    if (key.kid !== undefined && !map.has(key.kid)) {
+      map.set(key.kid, key);
+    }
+  }
+
+  return { keys: [...map.values()] };
+}
