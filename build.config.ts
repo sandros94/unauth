ee6ffd532a1b7037e256a852dfebeec1360e98e0
Sandro Circi <sandro.circi@digitoolmedia.com>
2026-05-13 11:32:58 +0200

    build: fix chunking

--- build.config.ts
+++ build.config.ts
@@ -5,16 +5,33 @@ export default defineBuildConfig({
   entries: [
     {
       type: "bundle",
-      input: ["./src/base/h3v1/index.ts", "./src/base/h3v2/index.ts"],
+      input: ["./src/base/h3v1/index.ts"],
       rolldown: {
-        platform: "neutral",
-        external: ["h3v1", "h3v2", "cookie-esv1", "cookie-esv3", "unjwt", "unsecure"],
+        platform: "neutral" as const,
+        external: ["h3v1", "cookie-esv1", "unjwt", "unsecure"],
         plugins: [
           replacePlugin(
             {
               '"h3v1"': '"h3"',
-              '"h3v2"': '"h3"',
               '"cookie-esv1"': '"cookie-es"',
+            },
+            {
+              delimiters: ["", ""],
+            },
+          ),
+        ],
+      },
+    },
+    {
+      type: "bundle",
+      input: ["./src/base/h3v2/index.ts"],
+      rolldown: {
+        platform: "neutral" as const,
+        external: ["h3v2", "cookie-esv3", "unjwt", "unsecure"],
+        plugins: [
+          replacePlugin(
+            {
+              '"h3v2"': '"h3"',
               '"cookie-esv3"': '"cookie-es"',
             },
             {
